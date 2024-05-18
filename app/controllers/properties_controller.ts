a5b174ef7b16a8c99c1c5050270e9cea43c8bf17
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2024-05-18 10:34:14 +0700

    feat: Added methods and endpoints for properties

--- app/controllers/properties_controller.ts
+++ app/controllers/properties_controller.ts
@@ -1,7 +1,13 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import Property from '#models/property'
+import PropertyService from '#services/property_service'
 
 export default class PropertiesController {
+  async index({ response, request }: HttpContext) {
+    const properties = PropertyService.getFiltered(request.qs())
+    return response.status(200).send(properties)
+  }
+
   async create({ request, response, auth }: HttpContext) {
     const user = auth.getUserOrFail()
     console.log(auth, auth.user, user)
@@ -13,6 +19,7 @@ export default class PropertiesController {
       'room_number',
       'bathroom_number',
       'floorNumber',
+      'is_sold',
       'metrics',
       'building_surface',
       'land_surface',
@@ -22,8 +29,36 @@ export default class PropertiesController {
     return response.status(201).send({ property, message: 'Property created successfully' })
   }
 
-  async index({ response }: HttpContext) {
-    const properties = await Property.query().preload('user')
-    return response.status(200).send(properties)
+  async delete({ params, response, auth }: HttpContext) {
+    const user = auth.getUserOrFail()
+    const property = await user.related('properties').query().where('id', params.id).firstOrFail()
+    await property.delete()
+    return response.status(200).send({ message: 'Property deleted successfully' })
+  }
+
+  async edit({ params, response, auth, request }: HttpContext) {
+    const user = auth.getUserOrFail()
+    const property = await user.related('properties').query().where('id', params.id).firstOrFail()
+    const data = request.only([
+      'name',
+      'description',
+      'price',
+      'location',
+      'room_number',
+      'bathroom_number',
+      'floorNumber',
+      'is_sold',
+      'metrics',
+      'building_surface',
+      'land_surface',
+    ])
+    console.log('actual data received', data)
+    property.merge(data).save()
+    return response.status(200).send({ message: 'Property modified successfully', data, property })
+  }
+
+  async show({ params, response }: HttpContext) {
+    const property = await Property.query().where('id', params.id).preload('user').firstOrFail()
+    return response.status(200).send(property)
   }
 }

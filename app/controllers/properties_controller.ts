72bab8a4e4a9a534b4c828e8fba35076059c27f1
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2024-05-18 11:29:10 +0700

    feat: Add support for adding tags to properties in create and edit endpoints

--- app/controllers/properties_controller.ts
+++ app/controllers/properties_controller.ts
@@ -1,6 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import Property from '#models/property'
 import PropertyService from '#services/property_service'
+import Tag from '#models/tag'
 
 export default class PropertiesController {
   async index({ response, request }: HttpContext) {
@@ -11,7 +12,7 @@ export default class PropertiesController {
   async create({ request, response, auth }: HttpContext) {
     const user = auth.getUserOrFail()
     console.log(auth, auth.user, user)
-    const data = request.only([
+    const data: Partial<Property> = request.only([
       'name',
       'description',
       'price',
@@ -19,12 +20,15 @@ export default class PropertiesController {
       'room_number',
       'bathroom_number',
       'floorNumber',
-      'is_sold',
-      'metrics',
+      'sold_at',
       'building_surface',
       'land_surface',
     ])
+    const tags: Tag[] = request.input('tags') || []
     const property = await Property.create(data)
+    if (tags.length) {
+      await property.related('tags').saveMany(tags)
+    }
     await user.related('properties').save(property)
     return response.status(201).send({ property, message: 'Property created successfully' })
   }
@@ -39,7 +43,7 @@ export default class PropertiesController {
   async edit({ params, response, auth, request }: HttpContext) {
     const user = auth.getUserOrFail()
     const property = await user.related('properties').query().where('id', params.id).firstOrFail()
-    const data = request.only([
+    const data: Partial<Property> = request.only([
       'name',
       'description',
       'price',
@@ -47,14 +51,17 @@ export default class PropertiesController {
       'room_number',
       'bathroom_number',
       'floorNumber',
-      'is_sold',
-      'metrics',
+      'sold_at',
       'building_surface',
       'land_surface',
     ])
     console.log('actual data received', data)
+    const tags: Tag[] = request.input('tags') || []
+    if (tags.length) {
+      await property.related('tags').saveMany(tags)
+    }
     property.merge(data).save()
-    return response.status(200).send({ message: 'Property modified successfully', data, property })
+    return response.status(200).send({ message: 'Property modified successfully', property })
   }
 
   async show({ params, response }: HttpContext) {

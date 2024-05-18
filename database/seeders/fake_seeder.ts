068648f8bca96ab384b0500fee4dcbf038b8d704
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2024-05-18 11:19:31 +0700

    chore: Update fake seeder to attach random tags to properties

--- database/seeders/fake_seeder.ts
+++ database/seeders/fake_seeder.ts
@@ -1,4 +1,5 @@
 import { PropertyFactory } from '#database/factories/property_factory'
+import { TagFactory } from '#database/factories/tag_factory'
 import { UserFactory } from '#database/factories/user_factory'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
@@ -6,6 +7,14 @@ export default class extends BaseSeeder {
   static environment = ['development', 'testing']
   async run() {
     await UserFactory.createMany(30)
-    await PropertyFactory.with('user').createMany(100)
+    const properties = await PropertyFactory.with('user').createMany(100)
+    const tags = await TagFactory.createMany(20)
+
+    for (const property of properties) {
+      const randomTags = tags
+        .sort(() => 0.5 - Math.random())
+        .slice(0, Math.floor(Math.random() * tags.length) + 1)
+      await property.related('tags').attach(randomTags.map((tag) => tag.id))
+    }
   }
 }
